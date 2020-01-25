a6402bb494fbd7f1aee123584497f413dfe8ecac
Ahmed Dif <ahmed.def.g@gmail.com>
2020-01-25 22:35:26 +0300

    factorial

--- src/ExtraLongFactorials.java
+++ src/ExtraLongFactorials.java
@@ -1,8 +1,20 @@
+//Complete the extraLongFactorials function in the editor below. It should print the result and return.
+//
+//        extraLongFactorials has the following parameter(s):
+//
+//        n: an integer
+//        Sample Input
+//        25
+//        Sample Output
+//        15511210043330985984000000
+
+
 
 import java.math.*;
 import java.util.*;
 public class ExtraLongFactorials {
     // Complete the extraLongFactorials function below.
+
     static void extraLongFactorials(int n) {
         BigInteger number =new BigInteger("1");
 
@@ -18,6 +30,7 @@ public class ExtraLongFactorials {
         int n = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
+        extraLongFactorials(n);
 
         scanner.close();
     }
